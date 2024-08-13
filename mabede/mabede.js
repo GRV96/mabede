c4efb69c7958e46c6cb369492f851dc19fd5725a
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-08-12 22:07:41 -0400

    Mabede displays weather data in the console.

--- mabede/mabede.js
+++ mabede/mabede.js
@@ -1,13 +1,20 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
 	res.status(200).send("A database accessed through a Node.js API");
 });
 
 app.post("/record-weather", (req, res) => {
-	console.log(`POST request body: ${req.body}`);
+	const reqBody = req.body;
+	console.log(`Moment: ${reqBody.moment}`);
+	console.log(`Temperature: ${reqBody.temperature}`);
+	console.log(`Precipitation probability: ${reqBody.preciProb}`);
+	console.log(`Wind speed: ${reqBody.windSpeed}`);
 	res.sendStatus(200);
 });
 
