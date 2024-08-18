9dc65ba5ae41db5a13162dcc3a0922eda5e3fe41
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-08-17 20:14:35 -0400

    Database service: port binding removed

--- mabede/src/database.js
+++ mabede/src/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
 	host: "database",
-	port: "3307",
+	port: "3306",
 	database: "mabede-database",
 	user: "utilisateur",
 	password: "mot-de-passe"
@@ -10,17 +10,19 @@ const pool = mysql.createPool({
 
 const promise = pool.promise();
 
+/*
 try {
-promise.execute(
-	"CREATE TABLE IF NOT EXISTS WeatherRecords (" +
-	"id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
-	"moment DATETIME NOT NULL," +
-	"temperature DOUBLE NOT NULL," +
-	"preciProb DOUBLE NOT NULL," +
-	"windSpeed DOUBLE NOT NULL);");}
+	//promise.execute("CREATE DATABASE IF NOT EXISTS mabede-database;");
+	promise.execute(
+		"CREATE TABLE IF NOT EXISTS WeatherRecords (" +
+		"id INT NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+		"moment DATETIME NOT NULL," +
+		"temperature DOUBLE NOT NULL," +
+		"preciProb DOUBLE NOT NULL," +
+		"windSpeed DOUBLE NOT NULL);");}
 catch (err) {
 	console.log(err);
-}
+}//*/
 console.log("Table created");
 
 function getWeatherRecords(startMoment, endMoment) {
