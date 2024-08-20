de95f4ce2a3a9d833724d4de0acdd7e96dca29a3
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-08-19 23:14:28 -0400

    Use of official environment variable MYSQL_TCP_PORT

--- mabede/src/database.js
+++ mabede/src/database.js
@@ -15,7 +15,7 @@ class MabedeError {
 
 const pool = mysql.createPool({
 	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
+	port: process.env.MYSQL_TCP_PORT,
 	database: process.env.MYSQL_DATABASE,
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD
